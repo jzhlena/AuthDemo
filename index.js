1e285ee80ee5be45c87fe39b299e18e79a3f0eea
starlysamax <149339085+starlysamax@users.noreply.github.com>
2024-08-27 14:31:59 -0400

    fix up user registration

--- index.js
+++ index.js
@@ -3,24 +3,41 @@ const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose')
 
+const bcrypt = require('bcrypt');
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-mongoose.connect('mongodb://localhost:27017/auth-demo', {
-});
+mongoose.connect('mongodb://localhost:27017/loginDemo')
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
 
-app.get('/register', (req, res)=>{
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) =>{
+    res.send('THIS IS THE HOME PAGE!')
+})
+
+app.get('/register', (req, res) => {
     res.render('register');
 })
 
-app.post('/register', async(req,res) =>{
-    res.send(req.body);
-} )
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12)
+    res.send(hash);
+    console.log(hash);
+})
 
 app.get('/secret', (req, res) => {
     res.send('THIS IS SECRET. YOU WILL NOT SEE ME UNLESS YOU LOG IN.')
 })
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("SERVING YOUR APP!");
 })
