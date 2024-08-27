5d9882d4a3eed6080bfd9a2af5a06aecb35458d1
starlysamax <149339085+starlysamax@users.noreply.github.com>
2024-08-27 16:40:11 -0400

    login functionality

--- index.js
+++ index.js
@@ -39,6 +39,22 @@ app.post('/register', async (req, res) => {
     res.redirect()
 })
 
+app.get('/login', (req, res)=>{
+    res.render('login');
+})
+
+app.post('/login', async(req, res) =>{
+    const {username, password} = req.body;
+    const user = await User.findOne({username});
+    const validPassword = await bcrypt.compare(password, user.password);
+    if(validPassword){
+        res.send("YAY WELCOME!")
+    }
+    else{
+        res.send("TRY AGAIN!")
+    }
+})
+
 app.get('/secret', (req, res) => {
     res.send('THIS IS SECRET. YOU WILL NOT SEE ME UNLESS YOU LOG IN.')
 })
