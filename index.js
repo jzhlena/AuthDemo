0bde76e4076bd2845adcca287873a6ed51000756
starlysamax <149339085+starlysamax@users.noreply.github.com>
2024-08-27 17:09:26 -0400

    express-session and staying logged in

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose')
-
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -17,7 +17,8 @@ mongoose.connect('mongodb://localhost:27017/loginDemo')
         console.log(err)
     })
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+app.use(session({secret: 'notagoodsecret'}));
 
 app.get('/', (req, res) =>{
     res.send('THIS IS THE HOME PAGE!')
@@ -35,8 +36,8 @@ app.post('/register', async (req, res) => {
         password: hash
     })
     await user.save();
-    res.send(hash);
-    res.redirect()
+    req.session.user_id = user.id;
+    res.redirect('/')
 })
 
 app.get('/login', (req, res)=>{
@@ -48,14 +49,18 @@ app.post('/login', async(req, res) =>{
     const user = await User.findOne({username});
     const validPassword = await bcrypt.compare(password, user.password);
     if(validPassword){
-        res.send("YAY WELCOME!")
+        req.session.user_id = user._id;
+        res.redirect('/secret');
     }
     else{
-        res.send("TRY AGAIN!")
+        res.redirect('/login');
     }
 })
 
 app.get('/secret', (req, res) => {
+    if(!req.session.user_id){
+        res.redirect('/login');
+    }
     res.send('THIS IS SECRET. YOU WILL NOT SEE ME UNLESS YOU LOG IN.')
 })
 
